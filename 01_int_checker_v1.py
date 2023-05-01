7056f900d65f8c0cf85f550fa347d53169d970bb
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2023-05-01 15:00:09 +1200

    Add files via upload

--- 01_int_checker_v1.py
+++ 01_int_checker_v1.py
@@ -1,6 +1,41 @@
 # Functions go here
-def int_checker():
-    
+def int_checker(question, low=None, high=None):
+    # Constant for function
+    situation = ""
 
+    # Sets variable for type of integer checking
+    if low is not None and high is not None:
+        situation = "both"
+
+    elif low is not None and high is None:
+        situation = "low only"
+
+    while True:
+
+        try:
+            response = int(input(question))
+
+            # Checks input is not too high or too low, if specified
+            if situation == "both":
+                if response < low or response > high:
+                    print(f"Please enter a number between {low} and {high}")
+                    continue
+
+            # Checks input is not too low
+            elif situation == "low only":
+                if response < low:
+                    print(f"Please enter a number that is more than (or equal to) {low}")
+                    continue
+
+            return response
+
+        # Checks input is an integer
+        except ValueError:
+            print("Please enter an integer")
+            continue
 
 # Main Routine goes here
+lowest = int_checker("Low: ")
+highest = int_checker("High: ", lowest + 1)
+rounds = int_checker("Rounds: ", 1)
+guess = int_checker("Guess: ", lowest, highest)
