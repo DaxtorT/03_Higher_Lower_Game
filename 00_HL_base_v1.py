ab2f0d22c36b5f0cfaa8c02163b1463c608dd71e
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2023-05-24 12:11:28 +1200

    Add files via upload

--- 00_HL_base_v1.py
+++ 00_HL_base_v1.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 # Functions go here
 def int_checker(question, low=None, high=None, exit_code=None):
@@ -75,6 +76,30 @@ def check_rounds():
 
         return response
   
+def statement_deco(deco_sides, deco_top_bottom, start_statement, three_line):
+    # Sets up decoration for sides of statement and top / bottom
+    sides = deco_sides * 3
+    single_statement = f"{sides} {start_statement} {sides}"
+
+    top_bottom = deco_top_bottom * len(single_statement)
+    big_statement = f"{top_bottom}\n{single_statement}\n{top_bottom}"
+
+    # Outputs either single or triple line statement
+    if three_line == 1:
+        return single_statement
+
+    elif three_line ==3:
+        return big_statement
+
+def instructions():
+    intro = statement_deco("*", "-", "How To Play", 1)
+    print(intro)
+    print("Playing Higher Lower is fairly simple")
+    print("All you have to do is choose a range of numbers that the program will choose a random 'Secret' number from")
+    print("You give it a 'Low Number' that can be any Whole Number, 0 or higher, then you give it a 'High Number' that is atleast 1 more than the 'Low Number'")
+    print("Then you choose a Number of Rounds to Play with that range (It will choose a new number every round)")
+    print("I Can't Be Bothered To Write Anymore Instruction")
+    return ""
 
 # Main Routine goes here
 # Constants for program
@@ -82,22 +107,42 @@ rounds_played = 0
 choose_instruction = "Please Guess a Number: "
 
 # Lists or Dicts for program
-
+yes_no_list = ["yes", "no"]
 
 # Welcome to game statement
-
+welcome_statement = statement_deco("=", "-", "Welcome to The Higher Lower Game", 3)
+print(welcome_statement)
 
 # Ask user if they have played before
-
+played_before = choice_checker("Have you played The Higher Lower Game before? ", "Please answer 'Yes' or 'No'", yes_no_list)
 
 # If 'yes', show instructions
+if played_before == "no" or played_before == "n":
+    print()
+    print(instructions())
 
+else:
+    print()
+
+# Play Game again loop
+play_again = "yes"
+while play_again == "yes":
+
+    # Now play the game
+    print("Now to actually play the game")
+    print()
 
     # Ask user for Low # (for secret # range)
     low_num = int_checker("Low Number: ")
 
     # Ask user for High # (for secret # range)
     high_num = int_checker("High Number: ", low_num + 1)
+
+    # Generate max allowed guesses
+    range = high_num - low_num + 1
+    max_raw = math.log2(range)
+    max_upped = math.ceil(max_raw)
+    max_guesses = max_upped + 1   
 
     # Ask user for # of rounds, <enter> for infinite mode
     rounds = int_checker("How Many Rounds? <Enter for Infinite> ", 1, exit_code="")
@@ -108,7 +153,10 @@ choose_instruction = "Please Guess a Number: "
         
         # Constants for loop
         guesses_played = 0
-        guesses_allowed = 10
+        guesses_left = 2
+
+        # Lists for loop
+        already_guessed = []
 
         # Generate random 'secret' number
         secret_num = random.randint(low_num, high_num)
@@ -128,13 +176,24 @@ choose_instruction = "Please Guess a Number: "
         while end_guess == "no":
 
             # This will be Main Game Code (Finish Later)
-            guess_heading = f"Guess {guesses_played + 1} of {guesses_allowed}"
+            guess_heading = f"Guess {guesses_played + 1} of {max_guesses}"
 
             print(guess_heading)
 
             # Checks intput is 
-            guess = int_checker("Please choose a number between 1 & 26: ", 1, 26, "xxx")
+            guess_question = f"Please choose a number between {low_num} & {high_num}: "
+            guess = int_checker(guess_question, low_num, high_num, "xxx")
 
+            # Checks that guess is not duplicate
+            if guess in already_guessed:
+                print("You have already guessed that number!",
+                    f"\nYou have {guesses_left} guesses left, please try again: ")
+                guesses_left -= 1
+
+            # Else append every guess to list
+            else:
+                already_guessed.append(guess)
+            
             # End game if exit code is typed
             if guess == "xxx":
                 end_round = "yes"
@@ -157,7 +216,7 @@ choose_instruction = "Please Guess a Number: "
             # Rest of loop / round
             guesses_played += 1
 
-            if guesses_played == guesses_allowed:
+            if guesses_played == max_guesses:
                 end_guess = "yes"
 
         # Rest of loop / game
@@ -166,11 +225,18 @@ choose_instruction = "Please Guess a Number: "
         if rounds_played == rounds:
                 end_round = "yes"
 
-
     # Tell user they have run out of rounds to play
-
-
-# Ask user if they want to play again
-
+    print()
+    print("You have Finished Playing the Rounds you asked for")
+    
+    # Ask user if they want to play again
+    ask_play_again = choice_checker("Do you want to play again? ", "Please answer 'Yes' or 'No'", yes_no_list)
+    if ask_play_again == "no" or ask_play_again == "n":
+        play_again = "no"
+    
+    # Print empty line for looks
+    print()
 
 # Thanks for Playing statement
+thanks_playing_statement = statement_deco("!", "-", "Thanks For Playing The Higher Lower Game", 3)
+print(thanks_playing_statement)
